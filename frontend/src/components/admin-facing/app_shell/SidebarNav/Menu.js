72b904c58eac328fa96bbbe0ae70db33b878fe99
Shakur Trammel <shakurPro@gmail.com>
2021-07-04 20:10:11 -0700

    Add sidebar navigation

---
+++ frontend/src/components/admin-facing/app_shell/SidebarNav/Menu.js
@@ -0,0 +1,121 @@
+import React from 'react'
+import styled from 'styled-components'
+import { NavLink } from 'react-router-dom'
+
+
+
+// Styled components.
+const DropdownLabel = styled.div`
+    padding: 0.5em 2em 0.5em 1.5em;
+    border: 1px solid #ccc;
+    background-color: #eee;
+    & ::after {
+        content: '';
+        position: absolute;
+        right: 1em;
+        top: 1em;
+        border: 0.3em solid;
+        border-color: black transparent transparent;
+    }
+`;
+
+const DropdownMenu = styled.div`
+    display: none;
+    position: absolute;
+    left: 0;
+    top: 2.1em;
+    min-width: 100%;
+    background-color: #eee;
+`;
+
+const DropdownContainer = styled.div`
+    display: inline-block;
+    position: relative;
+    width: 100%;
+    &:hover {
+        ${DropdownMenu} {
+            display: block;
+        }
+        ${DropdownLabel} ::after {
+            top: 0.7em;
+            border-color: transparent transparent black;
+        }
+        z-index: 1;
+    }
+`;
+
+const Submenu = styled.ul`
+    padding-left: 0;
+    margin: 0;
+    list-style-type: none;
+    border: 1px solid #999;
+`;
+
+const StyledLink = styled(NavLink)`
+    display: block;
+    padding: .5em 1.5em;
+    background-color: #eee;
+    color: #369;
+    text-decoration: none;
+    border-top: 1px solid #999;
+    &:hover {
+        background-color: #fff;
+    }
+`;
+
+
+
+// Main component.
+const Menu = [
+    {
+        label: 'Articles',
+        menu: <DropdownContainer id='dropdown-container'>
+                    <DropdownLabel id='dropdown-label'>
+                        <span>Articles</span>
+                    </DropdownLabel>
+                    <DropdownMenu id='dropdown-menu'>
+                        <Submenu id='submenu'>
+                            <StyledLink to='/admin'>Create Article</StyledLink>
+                            <StyledLink to='/admin'>Read Article</StyledLink>
+                            <StyledLink to='/admin'>Update Article</StyledLink>
+                            <StyledLink to='/admin'>Delete Article</StyledLink>
+                        </Submenu>
+                    </DropdownMenu>
+                </DropdownContainer>,
+    },
+    {
+        label: 'Analytics',
+        menu: <DropdownContainer id='dropdown-container'>
+                    <DropdownLabel id='dropdown-label'>
+                        <span>Analytics</span>
+                    </DropdownLabel>
+                    <DropdownMenu id='dropdown-menu'>
+                        <Submenu id='submenu'>
+                            <StyledLink to='/admin'>Create Article</StyledLink>
+                            <StyledLink to='/admin'>Read Article</StyledLink>
+                            <StyledLink to='/admin'>Update Article</StyledLink>
+                            <StyledLink to='/admin'>Delete Article</StyledLink>
+                        </Submenu>
+                    </DropdownMenu>
+                </DropdownContainer>,
+    },
+    {
+        label: 'Subscriptions',
+        menu: <DropdownContainer id='dropdown-container'>
+                    <DropdownLabel id='dropdown-label'>
+                        <span>Subscriptions</span>
+                    </DropdownLabel>
+                    <DropdownMenu id='dropdown-menu'>
+                        <Submenu id='submenu'>
+                            <StyledLink to='/admin'>Create Article</StyledLink>
+                            <StyledLink to='/admin'>Read Article</StyledLink>
+                            <StyledLink to='/admin'>Update Article</StyledLink>
+                            <StyledLink to='/admin'>Delete Article</StyledLink>
+                        </Submenu>
+                    </DropdownMenu>
+                </DropdownContainer>,
+    },
+
+]
+
+export default Menu;
--- frontend/src/components/admin-facing/app_shell/SidebarNav/index.js
+++ frontend/src/components/admin-facing/app_shell/SidebarNav/index.js
@@ -1,88 +1,29 @@
 import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
 import styled from 'styled-components'
-import { SidebarData } from './SidebarData.js'
+import Menu from './Menu.js'
 
 
 
-// Styles
-const NavContainer = styled.div`
-    width: 80px;
+// Styled components.
+const NavContainer = styled.nav`
+    width: 200px;
     height: 100vh;
     background-color: #2f4050;
 `;
 
-const StyledUL = styled.ul`
-    height: auto;
-    width: 100%;
-    padding: 0;
-`;
-
-const SubMenu = styled.ul`
-    position: absolute;
-    left: -80px;
-    transition: 0.3s;
-    list-style: none;
-`;
-
-const StyledLI = styled.li`
-    &:hover ${SubMenu} {
-        left: 100px;
-    }
-`;
-
-const StyledNavLink = styled(NavLink)`
-    height: 50px;
-    width: 100%;
-    list-style-type: none;
-    margin: 0px;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    color: white;
-    &:hover {
-        background-color: #293846; 
-    }
-`;
-
-const IconContainer = styled.div`
-    display: grid;
-    place-items: center;
-`;
-
-const TitleContainer = styled.div`
-    flex: 70%;
-`;
 
 
-
-
-
-// Export default component.
-class SidebarNav extends React.Component {
-
-    render() {
+// Main component.
+const SidebarNav = () => {
         return (
-            <NavContainer>
-                <StyledUL>
-                    {SidebarData.map( (item, key) => {
+                <NavContainer>
+                    {Menu.map((item, key) => {
                         return (
-                            <StyledLI key={key}>
-                                <StyledNavLink to={item.path}>
-                                    <IconContainer>{item.icon}</IconContainer>
-                                    <SubMenu>
-                                        <li><a href='/admin'>Category 1</a></li>
-                                        <li><a href='/admin'>Category 2</a></li>
-                                </SubMenu>
-                                </StyledNavLink>
-                            </StyledLI>
-                        );
+                           <div>{item.menu}</div>
+                        )
                     })}
-                </StyledUL>
-            </NavContainer>
+                </NavContainer>
         )
-    }
 }
 
 export default SidebarNav
